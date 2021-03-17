3fa03e3e9b4d5ad4a2fc2b3b95ab5ac87cdbd36a
Yumi Takahashi <roberta.takahashi@aluno.faculdadeimpacta.com.br>
2021-03-17 20:00:19 -0300

    att

--- scr/BancoApplication.java
+++ scr/BancoApplication.java
@@ -2,7 +2,7 @@ package scr;
 
 import java.util.Scanner;
 
-public class BancoAppV3 {
+public class BancoApplication {
     public static Scanner entrada;
 
     public static void mostrarInfo(BankAccount[] contas) {
@@ -13,12 +13,13 @@ public class BancoAppV3 {
         System.out.println("");
     }
 
-    public static void interacaoSacar(BankAccount[] contas) {
+    // -----------------------------------------------------------------
+
+    public static int ValidaCliente(BankAccount[] contas) {
         boolean clienteValido = false;
         int indiceConta = -1;
         while (!clienteValido) {
             mostrarInfo(contas);
-            System.out.print("O saque será efetuado na conta de qual cliente? (0 a " + (contas.length - 1) + "): ");
             indiceConta = entrada.nextInt();
             if (indiceConta >= 0 && indiceConta < contas.length) {
                 clienteValido = true;
@@ -27,21 +28,59 @@ public class BancoAppV3 {
             }
         }
 
+        return indiceConta;
+    }
+
+    // -----------------------------------------------------------------
+
+    public static void interacaoSacar(BankAccount[] contas) {
+        System.out.print("O saque será efetuado na conta de qual cliente? (0 a " + (contas.length - 1) + "): ");
+        int indiceConta = ValidaCliente(contas);
+
         System.out.print("Qual o valor do saque? ");
         double saque = entrada.nextDouble();
         contas[indiceConta].withDraw(saque);
         System.out.println("Saque finalizado.\n");
+
+        double taxa = saque * 0.25;
+        double cpmf = contas[indiceConta].getCpmf();
+        contas[indiceConta].setCpmf(cpmf + taxa);
     }
 
+    // -----------------------------------------------------------------
+
     public static void interacaoDepositar(BankAccount[] contas) {
-        System.out.println("Em construção...");
+        System.out.print("O depósito será efetuado na conta de qual cliente? (0 a " + (contas.length - 1) + "): ");
+        int indiceConta = ValidaCliente(contas);
+
+        System.out.print("Qual o valor do depósito? ");
+        double deposito = entrada.nextDouble();
+        contas[indiceConta].deposit(deposito);
+        System.out.println("Depósito finalizado.\n");
     }
+
+    // -----------------------------------------------------------------
+
+    public static void interacaoTransferir(BankAccount[] contas) {
+        System.out.print(
+                "O valor de transferência será debitado da conta de qual cliente? (0 a " + (contas.length - 1) + "): ");
+        int indiceConta1 = ValidaCliente(contas);
+        System.out.print("O valor de transferência será depositado na conta de qual cliente? (0 a "
+                + (contas.length - 1) + "): ");
+        int indiceConta2 = ValidaCliente(contas);
+
+        System.out.print("Qual o valor de tranferência? ");
+        double valor = entrada.nextDouble();
+        contas[indiceConta1].transfer(valor, contas[indiceConta2]);
+        System.out.println("Transferência finalizada.\n");
+    }
+
+    // -----------------------------------------------------------------
 
     public static void main(String[] args) {
         BankAccount[] contas = new BankAccount[5];
         contas[0] = new BankAccount("Marcos", 1000.00);
         contas[1] = new BankAccount("Júlia", 250.00);
-
         contas[2] = new BankAccount("João", 2500.00);
         contas[3] = new BankAccount("Roberto", 3000.00);
         contas[4] = new BankAccount("Janaína", 4500.00);
@@ -54,7 +93,8 @@ public class BancoAppV3 {
             System.out.println("(1) mostrar informações de todas as contas");
             System.out.println("(2) sacar");
             System.out.println("(3) depositar");
-            System.out.println("(4) sair");
+            System.out.println("(4) transferir");
+            System.out.println("(5) sair");
             System.out.print("Opção escolhida: ");
             int escolha = entrada.nextInt();
             System.out.println();
@@ -69,6 +109,9 @@ public class BancoAppV3 {
                 interacaoDepositar(contas);
                 break;
             case 4:
+                interacaoTransferir(contas);
+                break;
+            case 5:
                 sair = true;
                 break;
             default:
--- scr/BankAccount.java
+++ scr/BankAccount.java
@@ -1,10 +1,14 @@
 package scr;
 
+import java.util.Random;
+
 public class BankAccount {
     private static int lastAccountNumber = 1000; // Último número utilizado de conta
     private String owner;
     private int accountNumber; // número da conta
     private double balance; // saldo da conta
+    private double cpmf;
+    private String senha;
 
     public BankAccount(String owner) {
         // chama outro construtor dessa classe com os valores owner e 0, para saldo
@@ -14,7 +18,9 @@ public class BankAccount {
     public BankAccount(String owner, double balance) {
         accountNumber = lastAccountNumber++;
         this.balance = balance;
-        this.owner = owner;
+        this.owner = checkName(owner);
+        this.senha = makePassword();
+        this.cpmf = 0;
     }
 
     public void deposit(double amount) {
@@ -37,5 +43,47 @@ public class BankAccount {
 
     public String toString() {
         return "Conta de " + owner + " - Saldo de R$ " + balance;
+    }
+
+    public void transfer(double valor, BankAccount target) {
+        double newBalance = this.balance;
+        newBalance -= valor;
+        this.balance = newBalance;
+        target.balance += valor;
+    }
+
+    private static String checkName(String owner) {
+        boolean valida = owner.matches("\\w+[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]*");
+        if (valida == true) {
+            return owner;
+        } else {
+            System.exit(0);
+            return null;
+        }
+    }
+
+    public static String makePassword() {
+        String str = "abcdefghijklmnopqrstuvwxyz";
+        Random gerador = new Random();
+        String senha = "";
+
+        for (int i = 0; i < 3; i++) {
+            int x = gerador.nextInt(10);
+            senha = senha + str.charAt(x);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int x = gerador.nextInt(10);
+            senha = senha + x;
+        }
+        return senha;
+    }
+
+    public double getCpmf() {
+        return cpmf;
+    }
+
+    public void setCpmf(double cpmf) {
+        this.cpmf = cpmf;
     }
 }
